29c5f46277e35062d3f0a6683dc01e0a97afee92
Bruno Bernardi <brunobbf84@gmail.com>
2020-09-04 18:14:55 -0300

    Adjusting indentation

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,19 +36,19 @@ public class Program {
 			double salary = sc.nextDouble();
 			
 			List<String> emails = list.stream()
-							.filter(e -> e.getSalary() > salary)
-							.map(e -> e.getEmail())
-							.sorted()
-							.collect(Collectors.toList());
+					.filter(e -> e.getSalary() > salary)
+					.map(e -> e.getEmail())
+					.sorted()
+					.collect(Collectors.toList());
 			
 			System.out.println("Email of people whose salary is more than " + String.format("%.2f", salary) + ":");
 			emails.forEach(System.out::println);
 			
 			double sum = list.stream()
-							.filter(e -> e.getName().charAt(0) == 'M')
-							.map(e -> e.getSalary())
-							.reduce(0.0, (x, y) -> x + y);
-			
+					.filter(e -> e.getName().charAt(0) == 'M')
+					.map(e -> e.getSalary())
+					.reduce(0.0, (x, y) -> x + y);
+		
 			System.out.println("Sum of salary of people whose name starts with 'M': " + String.format("%.2f", sum));
 		}
 		catch (IOException e) {
